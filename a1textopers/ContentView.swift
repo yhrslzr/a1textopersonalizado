4b04d595de7c3b5703901408f53db816d149cc1d
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-01 17:53:18 -0600

    Initial Commit

---
+++ a1textopers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  a1textopers
+//
+//  Created by WIN603 on 01/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ a1textopers/a1textopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  a1textopersApp.swift
+//  a1textopers
+//
+//  Created by WIN603 on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct a1textopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
