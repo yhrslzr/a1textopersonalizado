29d72b8a1262da222b9916a7f0749e9bd26ee61c
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-01 18:08:10 -0600

    actividad 1 texto personalizado - fuente jedi 1

--- a1textopers/ContentView.swift
+++ a1textopers/ContentView.swift
@@ -1,21 +1,35 @@
 //
-//  ContentView.swift
-//  a1textopers
+//  fuentecita.swift
+//  hola mundito
 //
-//  Created by WIN603 on 01/09/25.
+//  Created by WIN603 on 15/08/25.
 //
 
 import SwiftUI
 
+let swtitle: String = String(localized: "swtitle")
+
+let swtext1: String = String(localized: "swtext1")
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(){
+            VStack(alignment: .center){
+                Text(swtitle).multilineTextAlignment(.center)
+            }
+            
+            VStack(alignment: .leading){
+                Text(swtext1)
+                    .rotation3DEffect(.degrees(45), axis: (x:1,y:0,z:0))
+                    .padding()
+            }
+            
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black, ignoresSafeAreaEdges:.bottom)
+            .font(.custom("starjedi",size: 15))
+                .foregroundStyle(.yellow)
+                .multilineTextAlignment(.leading)
     }
 }
 
